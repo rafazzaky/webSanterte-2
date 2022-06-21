558984780378edb87aea45e71db622ed72cb997f
rafazzaky <rafiazzakymc@gmail.com>
2022-06-21 10:24:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'ancient': ['Ancient', 'sans-serif'],
+        'akira': ['Akira', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
