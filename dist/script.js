181fb409ce97e218f78f20267c8cb41996ab3028
rafazzaky <rafiazzakymc@gmail.com>
2022-06-26 14:43:05 +0700

    adding changing new label background with random color

--- dist/script.js
+++ dist/script.js
@@ -10,3 +10,12 @@ window.onscroll = function(){
         header.classList.remove('navbar-fixed');
     }
 }
+
+//Random Color for 'New' Label
+//Get the root element (For Variable)
+var r = document.querySelector(':root');
+let newColors = ['#9333ea', '#4f46e5', '#2563eb', '#c026d3', '#e11d48'];
+function changeNewLabelColor(){
+    r.style.setProperty('--newColor', newColors[Math.floor(Math.random() * newColors.length)]);
+}
+setInterval(changeNewLabelColor, 250);
