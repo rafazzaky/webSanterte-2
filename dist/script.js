6171b95db159ad62958f605483562844f2c2d8d1
rafazzaky <rafiazzakymc@gmail.com>
2022-06-29 18:29:20 +0700

    Modal Box, Navbar Fixed

--- dist/script.js
+++ dist/script.js
@@ -14,12 +14,29 @@ window.onscroll = function(){
 // Hamburger
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
+let menuList = document.querySelectorAll('a.menu-list');
 
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
 
+for(let i = 0; i<menuList.length; i++){
+    menuList[i].addEventListener('click', function(){
+        if(hamburger.style.display == 'none'){
+            return;
+        }
+        hamburger.classList.toggle('hamburger-active');
+        navMenu.classList.toggle('hidden');
+    });
+}
+
+window.onresize = () => {
+    if ($(window).width() >= 768 && hamburger.classList.contains('hamburger-active')) {
+        hamburger.classList.toggle('hamburger-active');
+        navMenu.classList.toggle('hidden');
+    }
+}
 //Random Color for 'New' Label
 //Get the root element (For Variable)
 var r = document.querySelector(':root');
@@ -34,7 +51,7 @@ $(function() {
     // Desired offset, in pixels
     var refOffset = -57;
     // Desired time to scroll, in milliseconds
-    var scrollTime = 500;
+    var scrollTime = 300;
 
     $('a[href^="#"]').click(function() {
         // Need both `html` and `body` for full browser support
@@ -45,4 +62,34 @@ $(function() {
         // Prevent the jump/flash
         return false;
     });
+});
+
+//Modal Box
+let btnBaju = document.querySelectorAll('.card');
+let modalBaju = document.getElementById('bajuModal');
+let closeBaju = document.getElementById('closeBajuModal');
+console.log(btnBaju);
+for(let i = 0; i<btnBaju.length; i++){
+    btnBaju[i].addEventListener('click', ()=>{
+        modalBaju.classList.remove('hidden');
+        modalBaju.classList.add('block');
+    })
+}
+closeBaju.addEventListener('click', ()=>{
+    modalBaju.classList.add('hidden');
+    modalBaju.classList.remove('block');
+})
+
+window.onclick = function(event) {
+    if (event.target == modalBaju) {
+        modalBaju.classList.remove('block');
+        modalBaju.classList.add('hidden');
+    }
+  }
+
+$('.card').on('click', function() {
+    var linkTokped = $(this).data('tokped');
+    var linkShopee = $(this).data('shopee');
+    $('#bajuModal #link-tokped').attr("href", linkTokped);
+    $('#bajuModal #link-shopee').attr("href", linkShopee);
 });
